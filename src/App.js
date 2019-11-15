d5d452622ac6bf94a33768e89f674de4f5563ea7
Mindy Allen <mrsmallen07@gmail.com>
2019-11-15 12:08:22 -0500

    adjusting

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Header from "./Components/Header/header";
+import RegistrationForm from "./Components/Registration/RegistrationForm";
+import Login from "./Components/Login/login";
+import { BrowserRouter as Router, Route, } from "react-router-dom";
+
+function AppRouter() {
+  return (
+    <Router>
+      <div>
+        <Route exact path="/" exact component={Login} />
+        <Route path="/RegistrationForm/" component={RegistrationForm} />
+        <Route path="/Header/" component={Header} />
+      </div>
+    </Router>
+  );
+}
+
+export default AppRouter;
---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "../src/Assets/App.css";
+import App from "./App";
+import { BrowserRouter as Router, Route } from "react-router-dom"
+
+ReactDOM.render(
+  <Router>
+    <App />,
+    <Route />,
+  </Router>,
+  document.getElementById("root")
+);
+
+export default Router;
+
